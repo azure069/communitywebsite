04733239569da506845ae1dee28af836845e7819
azure069 <159586795+azure069@users.noreply.github.com>
2025-04-01 16:40:39 -0400

    Create page.tsx

---
+++ app/countdown/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Countdown from "@/components/countdown"; 
+import { Footer } from "@/components/footer"
+
+const CountdownPage: React.FC = () => {
+  return (
+    <div>
+      <Countdown />
+    </div>
+  );
+};
+
+export default CountdownPage;
