6ce5b0952fbb297fbc35e43336b654815b30cf38
azure069 <159586795+azure069@users.noreply.github.com>
2025-04-03 16:53:42 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,21 +7,21 @@ export const metadata: Metadata = {
   description: "Celebrating Nepali culture and building community connections in Belleville, Ontario.",
   icons: {
     icon: [
-      { url: "https://github.com/azure069/Comm-Web/blob/main/images/favicon.ico", sizes: "any" },
+      { url: "https://raw.githubusercontent.com/azure069/communitywebsite/master/public/favicon.ico", sizes: "any" },
       {
-        url: "https://github.com/azure069/Comm-Web/blob/main/images/favicon-16x16.png",
+        url: "https://raw.githubusercontent.com/azure069/communitywebsite/master/public/favicon-16x16.png",
         sizes: "16x16",
         type: "image/png",
       },
       {
-        url: "https://github.com/azure069/Comm-Web/blob/main/images/favicon-32x32.png",
+        url: "https://raw.githubusercontent.com/azure069/communitywebsite/master/public/favicon-32x32.png.png",
         sizes: "32x32",
         type: "image/png",
       },
     ],
     apple: [
       {
-        url: "https://github.com/azure069/communitywebsite/blob/master/public/apple-touch-icon.png",
+        url: "https://raw.githubusercontent.com/azure069/communitywebsite/master/public/apple-touch-icon.png",
         sizes: "180x180",
         type: "image/png",
       },
